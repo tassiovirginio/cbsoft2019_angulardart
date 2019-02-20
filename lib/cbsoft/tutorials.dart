d466d98658e2adcc80ebce6fa9a7a96eede960af
Tássio Virgínio <tassiovirginio@gmail.com>
2019-02-20 20:09:55 -0300

    melhorias

--- lib/cbsoft/tutorials.dart
+++ lib/cbsoft/tutorials.dart
@@ -1,11 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:cbsoft/src/components/page.dart';
 import 'package:cbsoft/src/route_paths.dart';
+import 'package:cbsoft/src/components/card.dart';
 
 @Component(
   selector: 'tutorials',
   templateUrl: 'tutorials.html',
-  directives: [Page]
+  directives: [Page,Card]
 )
 class Tutorials  {
   String url() => RoutePaths.tutorials.toUrl();
