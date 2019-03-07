19b4becae88fe3ee6cc59d6c570867d760adba19
Tássio Virgínio <tassiovirginio@gmail.com>
2019-03-07 13:59:03 -0300

    alterações workshop

--- lib/cbsoft/workshops.dart
+++ lib/cbsoft/workshops.dart
@@ -1,11 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:cbsoft/src/components/page.dart';
+import 'package:cbsoft/src/components/card.dart';
 import 'package:cbsoft/src/route_paths.dart';
 
 @Component(
   selector: 'workshops',
   templateUrl: 'workshops.html',
-  directives: [Page]
+  directives: [Page, Card]
 )
 class Workshops  {
   String url() => RoutePaths.workshops.toUrl();
