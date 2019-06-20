2eda9ce6b2f63d7ca4aa8f4c6e69ae660c858e36
Tássio Virgínio <tassiovirginio@gmail.com>
2019-06-20 16:29:34 -0300

    add keynote

--- lib/sast.dart
+++ lib/sast.dart
@@ -1,11 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:cbsoft/src/route_paths.dart';
 import 'package:cbsoft/src/components/page.dart';
+import 'package:cbsoft/src/components/card.dart';
 
 @Component(
   selector: 'sast',
   templateUrl: 'sast.html',
-  directives: [Page]
+  directives: [Page,Card]
 )
 class Sast {
   String url() => RoutePaths.sast.toUrl();
