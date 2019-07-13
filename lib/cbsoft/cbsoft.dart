743375a5eeed1602668db6b66de7c872ff293585
Tássio Virgínio <tassiovirginio@gmail.com>
2019-07-13 13:11:24 -0300

    melhorias

--- lib/cbsoft/cbsoft.dart
+++ lib/cbsoft/cbsoft.dart
@@ -1,11 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:cbsoft/src/components/page.dart';
 import 'package:cbsoft/src/route_paths.dart';
+import 'package:cbsoft/src/components/card.dart';
 
 @Component(
   selector: 'cbsoft',
   templateUrl: 'cbsoft.html',
-  directives: [Page]
+  directives: [Page,Card]
 )
 class CBSoft {
   String url() => RoutePaths.cbsoft.toUrl();
