be562a876da95481ab63a9b4e68e5e92931eec6d
Tássio Virgínio <tassiovirginio@gmail.com>
2019-08-09 16:18:08 -0300

    melhorias no calendario

--- lib/cbsoft/conferenceprogram.dart
+++ lib/cbsoft/conferenceprogram.dart
@@ -1,11 +1,13 @@
 import 'package:angular/angular.dart';
 import 'package:cbsoft/src/components/page.dart';
 import 'package:cbsoft/src/route_paths.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:ng_bootstrap/ng_bootstrap.dart';
 
 @Component(
   selector: 'conferenceprogram',
   templateUrl: 'conferenceprogram.html',
-  directives: [Page]
+  directives: [Page,bsDirectives,coreDirectives, formDirectives]
 )
 class Conferenceprogram {
   String url() => RoutePaths.conferenceprogram.toUrl();
